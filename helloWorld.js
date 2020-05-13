5ee4f547c72ac334cb56644d1763e2d0c8ed0b59
Jack Skiles <jackskiles420@gmail.com>
2020-05-13 16:07:08 -0400

    updates

--- helloWorld.js
+++ helloWorld.js
@@ -11,6 +11,10 @@ app.get("/dog", (req, res)=> res.send("Woof"));
 
 app.get("/cats_and_dogs", (req, res)=> res.send("Living together."));
 
+app.get("/greet/Jack", (req, res)=> res.send("Hello Jack!"));
+
+app.get("/greet/Jamison", (req, res)=> res.send("I want those mallaysia contracts."));
+
 app.listen(port, ()=>{
     console.log(`listening at http://localhost:${port}`);
 })
